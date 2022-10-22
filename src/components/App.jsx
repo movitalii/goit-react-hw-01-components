c04ae86adc7907e7e344f1b82ddfe42516600cbc
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-10-23 00:24:18 +0300

    add Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import Profile from './Profile/Profile'
+import user from '../data/user.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+ <Profile key={user.username}
+            avatar={user.avatar}
+            name={user.username}
+            tag={user.tag}
+            location={user.location}
+            stats={user.stats}
+        />
+  )
 };
