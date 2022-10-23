c87582979886ba6efdbf19f9025fa96d70752862
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-10-23 14:56:52 +0300

    Add styles for Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import Profile from './Profile/Profile'
+import css from './App.module.css';
 import user from '../data/user.json'
 
 export const App = () => {
   return (
- <Profile key={user.username}
+    <div className={css.wrapper}>
+      <Profile key={user.username}
             avatar={user.avatar}
             name={user.username}
             tag={user.tag}
             location={user.location}
             stats={user.stats}
         />
+    </div> 
   )
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,32 @@
 import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
 export default function Profile({ avatar, name, tag, location, stats }) {
     return (
-    <div class="profile">
-        <div class="description">
+    <div className={css.profile}>
+        <div className={css.description}>
             <img
                 src={avatar}
                 alt="User avatar"
-                class="avatar"
+                className={css.avatar}
             />
-            <p class="name">{name}</p>
-            <p class="tag">{tag}</p>
-            <p class="location">{location}</p>
+            <p className={css.name}>{name}</p>
+            <p className={css.tag}>@{tag}</p>
+            <p className={css.location}>{location}</p>
         </div>
 
-        <ul class="stats">
-            <li>
-                <span class="label">Followers</span>
-                <span class="quantity">{stats.followers}</span>
+        <ul className={css.stats}>
+            <li className={css.stats_item}>
+                <span className={css.label}>Followers</span>
+                <span className={css.quantity}>{stats.followers}</span>
             </li>
-            <li>
-                <span class="label">Views</span>
-                <span class="quantity">{stats.views}</span>
+            <li className={css.stats_item}>
+                <span className={css.label}>Views</span>
+                <span className={css.quantity}>{stats.views}</span>
             </li>
-            <li>
-                <span class="label">Likes</span>
-                <span class="quantity">{stats.likes}</span>
+            <li className={css.stats_item}>
+                <span className={css.label}>Likes</span>
+                <span className={css.quantity}>{stats.likes}</span>
             </li>
         </ul>
     </div>
