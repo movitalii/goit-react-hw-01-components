c42ad56ef8c8fb6b1aabb57a93b84a38db393f7d
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-10-23 21:42:05 +0300

    Add transactions

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export default function TransactionHistory({ transactions }) {
+    return (
+        <table className={css.transaction_history}>
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {transactions.map(({ id, type, amount, currency }) => (
+                    <tr key={id}>
+                        <td>{type}</td>
+                        <td>{amount}</td>
+                        <td>{currency}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+TransactionHistory.protoType = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired,      
+    })
+  ),
+};
